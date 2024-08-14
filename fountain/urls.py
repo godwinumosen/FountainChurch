fee17f34c9d9cdc127a7247a032d369047a1afa4
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-14 04:47:28 -0700

    Infrastructure and Public Works

--- fountain/urls.py
+++ fountain/urls.py
@@ -4,6 +4,7 @@ from .views import HomeView #ArticleDetailView
 
 urlpatterns = [
     path('', views.HomeView, name='home'),
+    #path('index/', views.index, name='index'),
     #path('', HomeView.as_view(), name="home"),
     #path('home/', HomeView.as_view(), name='home'),
     
