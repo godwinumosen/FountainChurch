f664f7e36ce00661dbef2a5c3f78c961237c589a
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-15 08:32:09 -0700

    Infrastructure and Public Works

--- fountain/admin.py
+++ fountain/admin.py
@@ -1,5 +1,12 @@
-# Register your models here.
 from django.contrib import admin
-from . import models 
-#from .models import Techbeatrice_Subjects,
-#from .models import 
+# Register your models here.
+from . import models
+from .models import FountainChurchMainPost
+
+
+
+#The Fountain Church Main Admin Post
+class FountainChurchMainPostModelAdmin (admin.ModelAdmin):
+    prepopulated_fields = {'fountain_church_slug': ('fountain_church_title',)}
+    list_display = ['fountain_church_title','fountain_church_author','fountain_church_img']
+admin.site.register(FountainChurchMainPost, FountainChurchMainPostModelAdmin)
--- fountain/models.py
+++ fountain/models.py
@@ -3,4 +3,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
-# Create your models here.
+#The search button model of locatin
+'''class Mainvideo(models.Model):
+    deus_magnus_first_video = models.FileField(upload_to='main_videos/') '''
+# The main model for Deus Magnus Model category
+class FountainChurchMainPost(models.Model):
+    fountain_church_title = models.CharField(max_length=255, blank=True, null=True)
+    fountain_church_description = models.TextField()
+    fountain_church_slug = models.SlugField (max_length=255,blank=True, null=True)
+    fountain_church_img = models.ImageField(upload_to='images/')
+    fountain_church_date = models.DateTimeField (auto_now_add= True)
+    fountain_church_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+       
+    class Meta:
+        ordering =['-fountain_church_date']
+    
+    def __str__(self):
+        return self.fountain_church_title + ' | ' + str(self.fountain_church_author)
+    
+    def get_absolute_url(self):
+        return reverse('home',)
