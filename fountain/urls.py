b04fe4cb25bcb365b7b29781dd2625f63f282bdd
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-19 01:59:04 -0700

    Cyber Security and Ethical hacking

--- fountain/urls.py
+++ fountain/urls.py
@@ -3,9 +3,9 @@ from . import views
 from .views import HomeView #ArticleDetailView
 
 urlpatterns = [
-    path('', views.HomeView, name='home'),
+    #path('', views.HomeView, name='home'),
     #path('index/', views.index, name='index'),
-    #path('', HomeView.as_view(), name="home"),
-    #path('home/', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name="home"),
+    path('home/', HomeView.as_view(), name='home'),
     
 ]
--- fountain/views.py
+++ fountain/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView,ListView
 from django.contrib.auth.decorators import login_required
-#from .models import Techbeatrice_Courses_PostModel,
+from .models import FountainChurchMainPost
 from django.contrib import messages
 from django.urls import reverse
 from django.urls import reverse_lazy
 # Create your views here.
 
-def HomeView (request):
-    return render (request, 'fountainchurch/home.html' )
+'''def HomeView (request):
+    return render (request, 'fountainchurch/home.html' )'''
 def base (request):
     return render(request,"base.html")
 
 #The main Techbeatrice HomeView page
-'''class HomeView(ListView): 
-    model = 
-    template_name =  '''
+class HomeView(ListView): 
+    model = FountainChurchMainPost
+    template_name = 'fountainchurch/home.html'
