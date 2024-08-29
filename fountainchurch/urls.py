df691adf8ca81bd16883d009abc4dfd1c67091cc
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-29 01:11:53 -0700

    The padding of each section is been change

--- fountainchurch/urls.py
+++ fountainchurch/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fountainchurch project.
+URL configuration for deus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('',include ('Fountain.urls')),
 ]
+
+admin.site.site_header = "Fountain Admin "
+admin.site.site_title = " Fountain Ltd "
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+
+
