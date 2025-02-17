7a3e0a8649289a826def0175ef3a9b1373728985
Godwin Umosen <umosengodwin568@gmail.com>
2025-02-17 14:09:22 +0100

    A new commit -m for the adjust of the class base V

--- fountain/views.py
+++ fountain/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView,ListView
 from django.contrib.auth.decorators import login_required
 from .models import FountainChurchMainPost,FountainChurchMainHeadImage,FountainChurchMinisterHome
-from .models import ChurchEvent, ChurchBlog, ChurchVideoSermons,ChurchGalary,Pastors,Contactvideo
+from .models import ChurchEvent, ChurchVideoSermons,ChurchGalary,Pastors,Contactvideo
 from .models import ChurchAudioSermons
 from django.contrib import messages
 from django.urls import reverse
